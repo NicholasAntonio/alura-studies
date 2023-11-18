e61b90dc13ead541806958c49ced716934626081
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-11-18 00:54:41 -0300

    initial commit - Button, Form, List and App modified. Styles sass and module added

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import style from './botao.module.scss'
+
+export function Botao(){
+    return(
+        <>
+        <button className={style.botao}>
+            Botão
+        </button>
+        </>
+    )
+}
---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import { Formulario } from "../components/Formulario"
+import { Lista } from "../components/Lista"
+import style from './app.module.scss'
+
+function App() {
+ 
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario />
+      <Lista />
+    </div>
+  )
+}
+
+export default App
